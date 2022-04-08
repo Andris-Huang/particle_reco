34972f8ccdab3f6698caeefc535e76b635273313
Andris Huang <andrewhz@berkeley.edu>
2022-04-08 01:09:05 -0700

    added QCL model

---
+++ src/models/QCL.py
@@ -0,0 +1,128 @@
+import importlib
+from qiskit import QuantumCircuit, assemble, transpile, Aer, IBMQ
+from qiskit.visualization import *
+from ibm_quantum_widgets import *
+from qiskit.providers.aer import QasmSimulator
+from qiskit.utils import QuantumInstance
+from qiskit.aqua.algorithms import VQC
+from qiskit.aqua.components.optimizers import SPSA, COBYLA
+#from qiskit.aqua.components.feature_maps import SecondOrderExpansion, FirstOrderExpansion
+#from qiskit.aqua.components.variational_forms import RYRZ
+from qiskit.circuit.library import ZZFeatureMap, TwoLocal, EfficientSU2
+from qiskit.aqua.utils import split_dataset_to_data_and_labels, map_label_to_class_name
+#from qiskit.aqua.input import ClassificationInput
+import numpy as np
+import matplotlib.pyplot as plt
+
+from qiskit.tools.visualization import plot_histogram
+
+utils = importlib.import_module("utils")
+base_file = importlib.import_module(f"src.models.base_QML")
+Base = base_file.Base
+
+class Model(Base):
+    """
+    Model Class for QCL
+    """
+    def __init__(self, config, save_fig=True, debug=False):
+        super().__init__(config)
+        self.base_params = {
+            "num_evt": None,
+            "num_job": 7,
+            "encoder_depth": 3,
+            "vqc_depth": 4,
+            "num_iter": 4,
+            "shots": 1024,
+            "optimizer": COBYLA,
+            "feature_map": ZZFeatureMap,
+            "var_form": EfficientSU2, # ansatz
+            "features": None, # Placeholder
+            "backend": "aer_simulator"
+        }
+        self.config = self._process_config(config)
+        self.training_input, self.test_input = self.process_data()
+        
+
+    def model(self):
+        """
+        Construct a VQC circuit model
+        """
+        # Get Hyperparameters
+        jobn = self.config['num_job']
+        backend_name = self.config['backend']
+        niter = self.config['num_iter']
+        feature_dim = len(self.config['features'])
+        shots = self.config['shots']
+        uin_depth = self.config['encorder_depth']
+        uvar_depth = self.config['vqc_depth']
+        Optimizer = self.config['optimizer']
+        featureMap = self.config['feature_map']
+        varForm = self.config['var_form']
+        random_seed = 10598+1010*uin_depth+101*uvar_depth+jobn
+        training_input = self.training_input
+        test_input = self.test_input
+        
+
+        # Construct Circuit
+        simulator = Aer.get_backend(backend_name)
+        optimizer = Optimizer(maxiter=niter, disp=True)
+        feature_map = featureMap(feature_dimension=feature_dim, reps=uin_depth)
+        var_form = varForm(num_qubits=feature_dim, reps=uvar_depth)
+        vqc = VQC(optimizer, feature_map, var_form, training_input, test_input)
+        self.quantum_instance = QuantumInstance(simulator, shots=shots, seed_simulator=random_seed, seed_transpiler=random_seed, skip_qobj_validation=True)
+        
+        return vqc
+
+    def train(self):
+        """
+        Train the QML model by running VQC
+        """
+        vqc = self.model()
+        self.result = vqc.run(self.quantum_instance)
+        counts = vqc.get_optimal_vector()
+        print("Counts (w/o noise) =",counts)
+        plot = plot_histogram(counts, title='Bit counts (w/o noise)')
+        if self.save_fig:
+            plot.savefig('Bit counts (w/o noise)' + '.png')
+        plot.show()
+        plot.clf()
+
+    def predict(self):
+        return self.model().predict
+
+    def evaluate(self):
+        datapoints, class_to_label = split_dataset_to_data_and_labels(self.test_input)
+        predicted_probs, predicted_labels = self.predict(datapoints[0])
+        predicted_classes = map_label_to_class_name(predicted_labels, self.model().label_to_class)
+
+        n_sig = np.sum(datapoints[1]==1)
+        n_bg = np.sum(datapoints[1]==0)
+        n_sig_match = np.sum(datapoints[1]+predicted_labels==2)
+        n_bg_match = np.sum(datapoints[1]+predicted_labels==0)
+
+        print(" --- Testing success ratio: ", self.result['testing_accuracy'],"(w/o noise)")
+        print(" ---   Signal eff =",n_sig_match/n_sig, ", Background eff =",(n_bg-n_bg_match)/n_bg, " (w/o noise)")
+
+
+        from sklearn.metrics import roc_curve, auc, roc_auc_score
+        prob_test_signal = predicted_probs[:,1]
+        fpr, tpr, thresholds = roc_curve(datapoints[1], prob_test_signal, drop_intermediate=False)
+        roc_auc = auc(fpr, tpr)
+        plt.plot(fpr, tpr, color='darkorange', lw=2, label='Testing w/o noise (area = %0.3f)' % roc_auc)
+        plt.xlim([-0.05, 1.0])
+        plt.ylim([0.0, 1.05])
+        plt.xlabel('False Positive Rate')
+        plt.ylabel('True Positive Rate')
+        plt.legend(loc="lower right")
+
+        datapoints_tr, class_to_label_tr = split_dataset_to_data_and_labels(self.training_input)
+        predicted_probs_tr, predicted_labels_tr = self.predict(datapoints_tr[0])
+        prob_train_signal = predicted_probs_tr[:,1]
+        fpr_tr, tpr_tr, thresholds_tr = roc_curve(datapoints_tr[1], prob_train_signal, drop_intermediate=False)
+        roc_auc_tr = auc(fpr_tr, tpr_tr)
+        plt.plot(fpr_tr, tpr_tr, color='darkblue', lw=2, label='Training w/o noise (area = %0.3f)' % roc_auc_tr)
+        plt.legend(loc="lower right")
+        plt.savefig('ROC.png')
+        plt.clf()
+        print(f'AUC(training) w/o noise = {roc_auc_tr:.3f}')
+        print(f'AUC(testing) w/o noise = {roc_auc:.3f}')
---
+++ src/models/base_QML.py
@@ -0,0 +1,59 @@
+import pandas as pd
+
+class QMLBase:
+    """
+    The base file for QML tasks.
+    """
+    def __init__(self, config, save_fig=True, debug=False):
+        self.base_params = {}
+        self.save_fig = save_fig
+        self.debug = debug
+        
+    
+    def _process_config(self, config):
+        new_config = config
+        for param in self.base_params:
+            if param not in self.config:
+                new_config[param] = self.base_params[param]
+        return new_config
+
+    def process_data(self):
+        
+        input_file = self.config["input_dir"]
+        df = pd.read_csv(input_file)
+        if "num_evt" not in self.config or self.config["num_evt"] is None:
+            self.nevt = self.config["num_evt"] = len(df)
+        else:
+            self.nevt = self.config["num_evt"]
+        if "training_size" not in self.config:
+            self.config["training_size"] = 0.7 * self.nevt
+        if "testing_size" not in self.config:
+            self.config["testing_size"] = 0.3 * self.nevt
+
+        SelectedFeatures = self.config["features"]
+        training_size = self.config["training_size"]
+        testing_size = self.config["testing_size"]
+
+        df_sig = df.loc[df.isSignal==1, SelectedFeatures]
+        df_bkg = df.loc[df.isSignal==0, SelectedFeatures]
+
+        df_sig_training = df_sig.values[:training_size]
+        df_bkg_training = df_bkg.values[:training_size]
+        df_sig_test = df_sig.values[training_size:training_size+testing_size]
+        df_bkg_test = df_bkg.values[training_size:training_size+testing_size]
+        training_input = {'1':df_sig_training, '0':df_bkg_training}
+        test_input = {'1':df_sig_test, '0':df_bkg_test}
+
+        return training_input, test_input
+
+    def model(self):
+        raise NotImplementedError
+
+    def train(self):
+        raise NotImplementedError
+
+    def predict(self):
+        raise NotImplementedError
+
+    def evaluate(self):
+        raise NotImplementedError
